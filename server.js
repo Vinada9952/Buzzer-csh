08782408f33beea0445591a865493404e9ce1ac0
Vincent <vincenttueminh@gmail.com>
2025-05-02 15:36:28 -0400

    a

--- server.js
+++ server.js
@@ -22,22 +22,22 @@ const io = new Server(server, {
     },
 });
 
-    const rooms = {}; // Stocker les salles et leurs joueurs
-    const playerNames = {}; // Associer les sockets aux noms des joueurs
+const rooms = {}; // Stocker les salles et leurs joueurs
+const playerNames = {}; // Associer les sockets aux noms des joueurs
 
-    io.on("connection", (socket) => {
-        console.log("Un client s'est connecté");
+io.on("connection", (socket) => {
+    console.log("Un client s'est connecté");
 
         socket.on("join-room", ( roomCode, playerName ) => {
             console.log( "room-code : ", roomCode );
 
             console.log(`Requête de connexion à la salle : ${roomCode} avec le nom : ${playerName}`);
 
-            if (!rooms[roomCode]) {
-                socket.emit("error", "Salle introuvable.");
-                return;
-            }
-        
+        if (!rooms[roomCode]) {
+            socket.emit("error", "Salle introuvable.");
+            return;
+        }
+
             console.log( "PlayerNames : ", playerNames );
             if( Object.values(playerNames).includes(playerName) )
             {
@@ -45,16 +45,16 @@ const io = new Server(server, {
             }
             else
             {
-                rooms[roomCode].push(socket.id);
+            rooms[roomCode].push(socket.id);
                 playerNames[socket.id] = playerName; // Associer le nom au socket
                 socket.join(roomCode); // Joindre le socket à la salle
                 console.log(`${playerName} a rejoint la salle : ${roomCode}`);
-            
-                const players = rooms[roomCode].map(id => playerNames[id]);
-                io.to(roomCode).emit("update-players", players);
-            }
-        });
-        
+
+            const players = rooms[roomCode].map(id => playerNames[id]);
+            io.to(roomCode).emit("update-players", players);
+        }
+    });
+
     socket.on("create-room", (code, playerName) => {
         if (rooms[code]) {
             socket.emit("room-used");
@@ -65,42 +65,42 @@ const io = new Server(server, {
 
             const players = rooms[code].map(id => playerNames[id]);
             io.to(code).emit("update-players", players);
-            }
-        });
+        }
+    });
 
-        socket.on("buzz", () => {
-            const roomCode = Object.keys(rooms).find(code => rooms[code].includes(socket.id));
-            if (roomCode) {
-                const playerName = playerNames[socket.id];
-                io.to(roomCode).emit("player-buzzed", `${playerName} a buzzé !`);
-            }
-        });
-        
+    socket.on("buzz", () => {
+        const roomCode = Object.keys(rooms).find(code => rooms[code].includes(socket.id));
+        if (roomCode) {
+            const playerName = playerNames[socket.id];
+            io.to(roomCode).emit("player-buzzed", `${playerName} a buzzé !`);
+        }
+    });
+
     socket.on("reset-buzz", () => {
-            const roomCode = Object.keys(rooms).find(code => rooms[code].includes(socket.id));
-            if (roomCode) {
+        const roomCode = Object.keys(rooms).find(code => rooms[code].includes(socket.id));
+        if (roomCode) {
             io.to(roomCode).emit("reset-buzz-state");
-            }
-        });
+        }
+    });
 
-        socket.on("disconnect", () => {
-            for (const roomCode in rooms) {
-                rooms[roomCode] = rooms[roomCode].filter(id => id !== socket.id);
+    socket.on("disconnect", () => {
+        for (const roomCode in rooms) {
+            rooms[roomCode] = rooms[roomCode].filter(id => id !== socket.id);
                 player = playerNames[socket.id];
-                delete playerNames[socket.id];
+            delete playerNames[socket.id];
 
             if (player === "host") {
                 io.to(roomCode).emit("delete-room");
-                    delete rooms[roomCode];
-                }
-                else
-                {
-                    const players = rooms[roomCode].map(id => playerNames[id]);
-                    io.to(roomCode).emit("update-players", players);
-                }
+                delete rooms[roomCode];
             }
-        });
+            else
+            {
+                const players = rooms[roomCode].map(id => playerNames[id]);
+                io.to(roomCode).emit("update-players", players);
+            }
+        }
     });
+});
 
 server.listen(port, () => {
     console.log(`Serveur en cours d'exécution sur le port ${port}`);
